dc1deff84896b91f26cfe012017fb8f2a218e9c5
ymvang89 <46541196+ymvang89@users.noreply.github.com>
2019-01-12 03:17:30 -0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Mitchell!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myNewTotal = 1000 - myTotal;
+
+        System.out.println(myNewTotal);
+
+    } // closes main
+} // closes class
